02806b7a50cbc61ef4ed8651aba09ee6077e245b
mohamedatef98 <eng.mohamedatef98@gmail.com>
2018-10-04 20:50:13 +0200

    Mass Assignment

--- app/Meeting.php
+++ app/Meeting.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Meeting extends Model
 {
-
+    protected $guarded = [];
 
     public function hasConfirmed(User $user){
         $confirmedUsers = explode(',',$this->users);
--- app/Submission.php
+++ app/Submission.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Submission extends Model
 {
+    protected $guarded = [];
     public function task(){
         return $this->belongsTo('\App\Task');
     }
