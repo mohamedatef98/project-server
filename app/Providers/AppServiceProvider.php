7d5029912a4956f9301eb785618edb88bdcbc9a0
mohamedatef98 <eng.mohamedatef98@gmail.com>
2018-10-08 18:24:09 +0200

    mail removed ; (

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        \App\Meeting::created(function ($meeting){
+        /*\App\Meeting::created(function ($meeting){
             foreach (\App\User::get() as $user){
                 Mail::to($user->email)->send(new MeetingsMail($meeting));
             }
@@ -27,7 +27,7 @@ class AppServiceProvider extends ServiceProvider
             foreach (\App\User::get() as $user){
                 Mail::to($user->email)->send(new TasksMail($task));
             }
-        });
+        });*/
     }
 
     /**
